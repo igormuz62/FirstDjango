fc9163ed6d48fe74a2e90fd3f00aae6fbc2ffa13
Игорь Музычук <immuzychuk@mts.ru>
2023-11-20 21:11:06 +0300

    2

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,3 +1,28 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+author = {"name": "Игорь",
+        "middle": "Михайлович",
+        "surname": "Музычук",
+        "phone": "8-910-447-21-02",
+        "email": "immuzychuk@mts.ru"
+          }
+
+
+
+def home(request):
+    text = """<h1>"Изучаем django"</h1>
+                <strong>Автор</strong>: <i>Музычук И.М.</i>
+            """
+    return HttpResponse(text)
+
+def about(request):
+    text = f"""Имя: <b>{author["name"]}</b><br>
+        Отчество: <b>{author["middle"]}</b><br>
+        Фамилия: <b>{author["surname"]}</b><br>
+        телефон: <b>{author["phone"]}</b><br>
+        email: <b>{author["email"]}</b><br>
+        """
+    return HttpResponse(text)
